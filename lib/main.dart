8f4012c4d09a287fb9d8fcfe343a8e5aa4651af3
rkdtnlzl <rkdtnlzl@naver.com>
2022-02-28 17:23:32 +0900

    2/28 second commit

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'changhyun is gold'),
+      home: const MyHomePage(title: 'changhyun'),
     );
   }
 }
